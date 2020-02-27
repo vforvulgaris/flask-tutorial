5dcce9a7ea359c0d83c81ecbf39f2886c0a2ca64
_V <v@v.com>
2020-02-27 03:54:52 +0300

    temp commit for chapter 6

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -20,6 +21,9 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=ideticon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
